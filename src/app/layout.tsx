612a7dbd11b15ab3fa485c6521648dfea1dbd0f7
ShuvrotoKumar <sdks6409@gmail.com>
2026-03-08 23:18:47 +0600

    fixed all ? Done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default async function RootLayout({
       <body className={`${poppins.variable} bg-background font-sans antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
